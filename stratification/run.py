922bde97e59bc658eb527834d84d5299218725ae
abdul786rasool <120364251+abdul786rasool@users.noreply.github.com>
2024-06-16 12:39:34 +0530

    Update run.py

--- stratification/run.py
+++ stratification/run.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import json
 
 from stratification.harness import GEORGEHarness
 from stratification.utils.utils import set_seed, init_cuda
@@ -15,7 +16,7 @@ def main():
     torch.multiprocessing.set_sharing_strategy('file_system')
     harness = GEORGEHarness(config, use_cuda=use_cuda)
     harness.save_full_config(config)
-    print(config)
+    print(json.dumps(config,indent=4))
     first_mode = 'erm' if (config['mode'] == 'george') else config['mode']
     dataloaders = harness.get_dataloaders(config, mode=first_mode)
     num_classes = dataloaders['train'].dataset.get_num_classes('superclass')
