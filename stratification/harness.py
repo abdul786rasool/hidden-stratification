43bf10bc43305dcd7824afa96e7c8fff4b26e2e7
abdul786rasool <120364251+abdul786rasool@users.noreply.github.com>
2024-07-04 23:06:08 +0530

    Update harness.py

--- stratification/harness.py
+++ stratification/harness.py
@@ -379,7 +379,7 @@ class GEORGEHarness:
             mode_config = cl_config[f'gdro_config']
         cl_config = merge_dicts(cl_config, mode_config)
         if cl_config['bit_pretrained']:
-            model_cls = Dino_Model
+            model_cls = BiTResNet
         else:
             models = {'lenet4': LeNet4, 'resnet50': PyTorchResNet, 'shallow_cnn': ShallowCNN}
             try:
