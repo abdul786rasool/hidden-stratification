ba641af95343651b6c5b7c54a286ff85d1179548
abdul786rasool <120364251+abdul786rasool@users.noreply.github.com>
2024-07-31 20:08:04 +0530

    Update cifar_ten.py

--- stratification/classification/datasets/cifar_ten.py
+++ stratification/classification/datasets/cifar_ten.py
@@ -202,7 +202,7 @@ def get_transform_CIFAR(resize=True, augment=False):
         transforms.Normalize(**CifarTenDataset._normalization_stats)
     ]
     if resize:
-        test_transform_list.insert(0, transforms.Resize((32, 32)))
+        test_transform_list.insert(0, transforms.Resize((518, 518)))
     if not augment:
         return transforms.Compose(test_transform_list)
 
