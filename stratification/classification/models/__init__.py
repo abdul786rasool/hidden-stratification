85e13c5b24da33c20099051fcf53df1d14c0fb03
abdul786rasool <120364251+abdul786rasool@users.noreply.github.com>
2024-10-07 18:40:25 +0530

    Update __init__.py

--- stratification/classification/models/__init__.py
+++ stratification/classification/models/__init__.py
@@ -3,3 +3,4 @@ from .shallow_cnn import ShallowCNN
 from .pt_resnet import PyTorchResNet
 from .bit_pytorch_models import BiTResNet
 from .dino_models import Dino_Model
+from .llama2 import llama2, collate_fn_llama2
