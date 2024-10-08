c519c8fb506f750482631c2f78024d3ddca9afb2
abdul786rasool <120364251+abdul786rasool@users.noreply.github.com>
2024-10-08 12:57:46 +0530

    Update __init__.py

--- stratification/classification/models/__init__.py
+++ stratification/classification/models/__init__.py
@@ -4,3 +4,4 @@ from .pt_resnet import PyTorchResNet
 from .bit_pytorch_models import BiTResNet
 from .dino_models import Dino_Model
 from .llama2 import llama2, collate_fn_llama2
+from .SentenceTransformer import SentenceTransformer, collate_fn_SentenceTransformer
