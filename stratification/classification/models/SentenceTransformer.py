3aff95bcc6ec6b518b28716f9ad8e3a0ec9b1b6f
abdul786rasool <120364251+abdul786rasool@users.noreply.github.com>
2024-10-08 16:42:44 +0530

    Update SentenceTransformer.py

--- stratification/classification/models/SentenceTransformer.py
+++ stratification/classification/models/SentenceTransformer.py
@@ -22,7 +22,7 @@ class SentenceTransformerForClassification(nn.Module):
 
 def collate_fn_SentenceTransformer(batch):
     # Tokenize each text in the batch
-    texts, y_dict = zip(*batch)
+    texts, y_dicts = zip(*batch)
     tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
     tokenized_batch = tokenizer(
         list(texts),  # List of texts
@@ -30,13 +30,20 @@ def collate_fn_SentenceTransformer(batch):
         padding=True,  # Perform padding here
         return_tensors='pt'
     )
-
+    merged_y_dict = {}
+    for y_dict in y_dicts:
+        for key, value in y_dict.items():
+            if key not in merged_y_dict:
+                merged_y_dict[key] = []
+            merged_y_dict[key].append(value.item())    
+    for key,value in y_dict.items():
+        merged_y_dict[key] = torch.tensor(merged_y_dict[key])
     return {
         'input_ids': tokenized_batch['input_ids'],
         'token_type_ids':tokenized_batch['token_type_ids'],
         'attention_mask': tokenized_batch['attention_mask'],
         
-    }, y_dict
+    }, merged_y_dict
 
 def SentenceTransformer(**kwargs):
     model = SentenceTransformerForClassification(**kwargs)
