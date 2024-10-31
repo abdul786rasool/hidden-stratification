75ef2f9bed678f2497fe238b829648e7c27a94cf
abdul786rasool <120364251+abdul786rasool@users.noreply.github.com>
2024-10-31 11:12:44 +0530

    Update __init__.py

--- stratification/classification/models/__init__.py
+++ stratification/classification/models/__init__.py
@@ -5,3 +5,4 @@ from .bit_pytorch_models import BiTResNet
 from .dino_models import Dino_Model
 from .llama2 import llama2, collate_fn_llama2
 from .SentenceTransformer import SentenceTransformer, collate_fn_SentenceTransformer
+from .minilm import miniLM, collate_fn_miniLM
